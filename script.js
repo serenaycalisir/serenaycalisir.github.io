8f63badb0bf44cc63e1688c004df7d21cb4b2b39
SerenayClsr <Srnyclsr@hotmail.com>
2023-03-30 11:35:12 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function Tikla(){
+    alert("Kod dosyasından çalıştı.");
+}
+
+function buton(){
+    alert("Websiteme Hoşgeldiniz");
+}
+
+function medium(){
+    window.location.href = "https://www.google.com";
+  }
+
+  function RenkDegistir() {
+    var element = document.getElementById("p1");
+
+    element.style.color = "purple";
+}
